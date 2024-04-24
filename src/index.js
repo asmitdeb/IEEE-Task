478cf7ddb9bb6af856b975d6e18971649dddd36c
Asmit Deb <47671715+asmitdeb@users.noreply.github.com>
2024-04-24 15:39:30 +0530

    removed vercel

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: false})); //needed?
 // app.use(express.static(__dirname + '/../public'));
 app.set("view engine", "hbs");
 
